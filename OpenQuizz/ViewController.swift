fcb8fc66c843e8c1e7d31eda8227c7dc994d3e04
Axel Froehlicher <axel.froehlicher@telecomnancy.eu>
2017-12-24 02:24:51 +0100

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Froehlicher Axel on 24/12/2017.
+//  Copyright © 2017 Froehlicher Axel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
