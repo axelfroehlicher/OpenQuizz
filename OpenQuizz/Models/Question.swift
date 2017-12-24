b3b1e9fabeaf8fc57ffab731614b212407cc99c3
Axel Froehlicher <axel.froehlicher@telecomnancy.eu>
2017-12-24 03:28:18 +0100

    Initialisation Création LaunchScreen Model Question Organisation projet

---
+++ OpenQuizz/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Froehlicher Axel on 24/12/2017.
+//  Copyright © 2017 Froehlicher Axel. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var title: String = ""
+    var isCorrect: Bool = false
+}
