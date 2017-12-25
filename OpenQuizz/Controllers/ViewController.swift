127db1a2ac7e6540eb27010b84a049b9cbb95ce2
Axel Froehlicher <axel.froehlicher@telecomnancy.eu>
2017-12-25 13:43:54 +0100

    Application end

--- OpenQuizz/Controllers/ViewController.swift
+++ OpenQuizz/Controllers/ViewController.swift
@@ -10,9 +10,140 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var questionView: QuestionView!
+    
+    var game = Game()
+    
+    @IBAction func tapNewGameButton() {
+        startNewGame()
+    }
+    
+    private func startNewGame() {
+        newGameButton.isHidden = true
+        activityIndicator.isHidden = false
+        
+        questionView.title = "Loading ..."
+        questionView.style = .standard
+        
+        scoreLabel.text = "0 / 10"
+        game.refresh()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let name = Notification.Name(rawValue: "QuestionsLoaded")
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(questionsLoaded),
+            name: name, object: nil)
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragQuestionView(_:)))
+        questionView.addGestureRecognizer(panGestureRecognizer)
+        
+        let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(dragView(_:)))
+        swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirection.right
+        mainView.addGestureRecognizer(swipeGestureRecognizer)
+        
+        startNewGame()
+    }
+    
+    @objc func dragView(_ sender: UIScreenEdgePanGestureRecognizer){
+        switch sender.state{
+        case .ended, .cancelled:
+            startNewGame()
+        default:
+            break
+        }
+    }
+    
+    @objc func dragQuestionView(_ sender: UIPanGestureRecognizer) {
+        if game.state == .ongoing {
+            switch sender.state {
+            case .began, .changed:
+                transformQuestionViewWith(gesture: sender)
+            case .ended, .cancelled:
+                answerQuestion()
+            default:
+                break
+            }
+        }
+    }
+    
+    private func transformQuestionViewWith(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: questionView)
+        
+        let translationTransform = CGAffineTransform(translationX: translation.x, y: 0)
+        
+        let translationPercent = translation.x/(UIScreen.main.bounds.width / 2)
+        let rotationAngle = (CGFloat.pi / 3) * translationPercent
+        let rotationTransform = CGAffineTransform(rotationAngle: rotationAngle)
+        
+        let transform = translationTransform.concatenating(rotationTransform)
+        questionView.transform = transform
+        
+        if translation.x > 0 {
+            questionView.style = .correct
+        } else {
+            questionView.style = .incorrect
+        }
+        
+    }
+    
+    private func answerQuestion() {
+        switch questionView.style {
+        case .correct:
+            game.answerCurrentQuestion(with: true)
+        case .incorrect:
+            game.answerCurrentQuestion(with: false)
+        case .standard:
+            break
+        }
+        
+        scoreLabel.text = "\(game.score) / 10"
+        
+        let screenWidth = UIScreen.main.bounds.width
+        var translationTransform: CGAffineTransform
+        if questionView.style == .correct {
+            translationTransform = CGAffineTransform(translationX: screenWidth, y: 0)
+        } else {
+            translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.questionView.transform = translationTransform
+        }, completion: { (success) in
+            if success {
+                self.showQuestionView()
+            }
+        })
+        
+    }
+    
+    private func showQuestionView() {
+        questionView.transform = .identity
+        questionView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        
+        questionView.style = .standard
+        
+        switch game.state {
+        case .ongoing:
+            questionView.title = game.currentQuestion.title
+        case .over:
+            questionView.title = "Game Over"
+        }
+        
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+            self.questionView.transform = .identity
+        }, completion:nil)
+    }
+    
+    @objc func questionsLoaded() {
+        newGameButton.isHidden = false
+        activityIndicator.isHidden = true
+        questionView.title = game.currentQuestion.title
     }
 
     override func didReceiveMemoryWarning() {
